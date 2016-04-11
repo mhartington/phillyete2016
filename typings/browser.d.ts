3ddcdd9205a8d4723fbc49a777befc76638d942b
mhartington <mikehartington@gmail.com>
2016-04-11 09:29:20 -0400

    remove typings

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="browser/ambient/marked/index.d.ts" />
-/// <reference path="browser/definitions/lodash/index.d.ts" />
-/// <reference path="browser/definitions/moment/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/marked/index.d.ts" />
-/// <reference path="main/definitions/lodash/index.d.ts" />
-/// <reference path="main/definitions/moment/index.d.ts" />
